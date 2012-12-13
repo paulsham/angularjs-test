bf88900b7dff143f2c5513bb190cf14d82eefed2
Paul Sham <contact@paulsham.com>
2012-12-13 17:50:45 -0500

    Yoeman angular init

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var angularjsTestApp = angular.module('angularjsTestApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angularjsTestApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
